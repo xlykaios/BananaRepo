5077d258dcb79f684ce63fb885b3f7650f0bd7b5
egrishina <egrishina22@fed.idserve.net>
2022-10-24 13:22:16 +0200

    View WIP

--- Taskable/Taskable/Views/NewTask/NewTaskView.swift
+++ Taskable/Taskable/Views/NewTask/NewTaskView.swift
@@ -14,51 +14,117 @@ struct NewTaskView: View {
         static let notesPlaceholder = "Notes"
         
         static let deadlineTitle = "Deadline"
+        static let dateTitle = "Date"
         static let priorityTitle = "Priority"
         static let tagsTitle = "Tags"
+        
+        static let backgroundColor = Color(red: 0.141, green: 0.141, blue: 0.149)
+        static let accentColor = Color(red: 0.11, green: 0.11, blue: 0.118)
     }
     
-    @Binding var task: Tasky
-    @State private var hasDeadline: Bool = false
+    @State private var taskName = ""
+    @State private var taskNotes = ""
+    @State private var taskPriority: Priority = .normal
+    @State private var hasDeadline: Bool = true
+    @State private var date = Date()
     
     var body: some View {
         NavigationView {
-            VStack {
-                
-                // MARK: - Text input section
-                TextField(Const.titlePlaceholder, text: $task.name)
-                TextField(Const.notesPlaceholder, text: $task.bio)
-                
-                // MARK: - Deadline section
-                HStack {
+            ZStack {
+                Const.backgroundColor.ignoresSafeArea(.all)
+                VStack {
+                    
+                    // MARK: - Text input section
+                    VStack {
+                        TextField(Const.titlePlaceholder, text: $taskName)
+                            .padding([.horizontal, .top], 16)
+                        .cornerRadius(8)
+                        Divider()
+                        TextField(Const.notesPlaceholder, text: $taskNotes)
+                            .padding([.horizontal, .bottom], 16)
+                            
+                    }
+                    .background(Const.accentColor)
+                    .cornerRadius(8)
+                    
+                    
+                    // MARK: - Deadline section
                     Toggle(isOn: $hasDeadline) {
                         Text(Const.deadlineTitle)
                     }
-                }
-                
-                // MARK: - Priority section
-                HStack {
-                    Text(Const.priorityTitle)
+                    .padding([.horizontal], 16)
+                    .padding([.vertical], 11)
+                    .background(Const.accentColor)
+                    .cornerRadius(8)
+                    
+                    // MARK: - Datetime section
+                    if hasDeadline {
+                        VStack {
+                            DatePicker(
+                                Const.dateTitle,
+                                selection: $date,
+                                in: Date()...
+                            )
+                            .foregroundColor(.white)
+                            .padding([.horizontal], 9)
+                            .datePickerStyle(.graphical)
+                            .background(Const.accentColor, in: RoundedRectangle(cornerRadius: 20))
+                        }
+                    }
+                    
+                    // MARK: - Priority section
+                    HStack {
+                        Text(Const.priorityTitle)
+                        Spacer()
+                        Text(taskPriority.rawValue)
+                    }
+                    .padding(13)
+                    .background(Const.accentColor)
+                    .cornerRadius(8)
+                    
+                    // MARK: - Tags section
+                    HStack {
+                        Text(Const.tagsTitle)
+                        Spacer()
+                    }
+                    .padding(13)
+                    .background(Const.accentColor)
+                    .cornerRadius(8)
+                    
                     Spacer()
-                    Text(task.priority.rawValue)
                 }
-                
-                // MARK: - Tags section
-                HStack {
-                    Text(Const.tagsTitle)
-                    Spacer()
-                }
-                
-                Spacer()
+                .padding(12)
             }
-            .navigationTitle("New Task")
-            .padding(16)
+            .foregroundColor(.white)
+            // MARK: - Navigation setup
+            .navigationTitle("New Task").foregroundColor(.white)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Close") {
+                        print("Tapped close")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        print("Tapped done")
+                    }
+                }
+            }
         }
+    }
+    
+    init() {
+//        super.init()
+        //Use this if NavigationBarTitle is with Large Font
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        //Use this if NavigationBarTitle is with displayMode = .inline
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
     }
 }
 
 struct NewTaskView_Previews: PreviewProvider {
     static var previews: some View {
-        NewTaskView(task: .constant(Tasky.emptyTask()))
+        NewTaskView()
     }
 }
