a2d73dca780e0567d0b8c35da7a334251d07b731
Lykaxx <xlykaios@gmail.com>
2022-10-24 10:18:31 +0200

    First Commit

---
+++ Taskable/Taskable/TaskableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskableApp.swift
+//  Taskable
+//
+//  Created by Antonio Giordano on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
