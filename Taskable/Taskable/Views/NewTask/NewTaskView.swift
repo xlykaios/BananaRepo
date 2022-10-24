597a30fd36cd445c50299d64d0eae705986418aa
egrishina <egrishina22@fed.idserve.net>
2022-10-24 10:51:02 +0200

    New task

---
+++ Taskable/Taskable/Views/NewTask/NewTaskView.swift
@@ -0,0 +1,19 @@
+//
+//  NewTaskView.swift
+//  Taskable
+//
+//  Created by Ekaterina Grishina on 24/10/22.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }.navigationTitle("New Task")
+        }
+    }
+}
